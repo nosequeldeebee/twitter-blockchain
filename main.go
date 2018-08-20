bdaf70e0c61f5fc6f5a3cb2871f501715aa777d3
nosequeldeebee <nosequeldeebee@users.noreply.github.com>
2018-08-20 07:53:39 -0700

    add gitignore and more comments

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ type Tweet struct {
 var config *oauth1.Config
 var token *oauth1.Token
 
+// API page limit
 const pages = 18
 
 func main() {
@@ -34,6 +35,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Load API credentials
 	config = oauth1.NewConfig(os.Getenv("APIKEY"), os.Getenv("APISECRET"))
 	token = oauth1.NewToken(os.Getenv("TOKEN"), os.Getenv("TOKENSECRET"))
 
@@ -50,7 +52,7 @@ func main() {
 	}
 }
 
-// MakeMuxRouter defines and creates routes
+// makeMuxRouter defines and creates routes
 func makeMuxRouter() http.Handler {
 	muxRouter := mux.NewRouter()
 	muxRouter.HandleFunc("/{id}", handleGetTweets).Methods("GET")
@@ -74,9 +76,11 @@ func handleGetTweets(w http.ResponseWriter, r *http.Request) {
 
 Outer:
 	for i := 0; i < pages; i++ {
-		// example Twitter API request
+		// Twitter API request
+		// userID is the Twitter hand
+		// maxIDQuery is the last result on each page, so the API knows what the next page is
 		path := fmt.Sprintf("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%v&include_rts=false&count=200%v", userID, maxIDQuery)
-		if strings.Contains(path, "favicon.ico") {
+		if strings.Contains(path, "favicon.ico") { // API returns this so skip it, not needed
 			break
 		}
 
@@ -100,15 +104,18 @@ Outer:
 			break
 		}
 
+		// range through Tweets to clear out unneeded info
 		for i, t := range gotTweets {
 
 			if i == len(gotTweets)-1 {
+				// this is the logic to tell Twitter API where the pages are
 				if maxIDQuery == fmt.Sprintf("&max_id=%v", t.ID) {
 					break Outer
 				}
 				maxIDQuery = fmt.Sprintf("&max_id=%v", t.ID)
 			}
 
+			// remove @ mentions and links from returned Tweets
 			regAt := regexp.MustCompile(`@(\S+)`)
 			t.Text = regAt.ReplaceAllString(t.Text, "")
 			regHTTP := regexp.MustCompile(`http(\S+)`)
